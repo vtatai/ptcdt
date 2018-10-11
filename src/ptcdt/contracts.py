2fd6b28313441cf0246c57bd212af981a439419c
Victor Tatai <vtatai@fitbit.com>
2018-10-10 20:17:32 -0700

    Intermediary commit for pact file support

--- src/ptcdt/contracts.py
+++ src/ptcdt/contracts.py
@@ -1,3 +1,74 @@
+import json
+
+def parse_contract(filename):
+    with open(filename) as f:
+        d = json.load(f)
+        return Contract.from_dict(d)
+
+class Contract:
+    def __init__(self, provider, consumer, interactions=[]):
+        self.provider = provider
+        self.consumer = consumer
+        self.interactions = interactions
+
+    def add_interaction(self, interaction):
+        self.interactions.append(interaction)
+
+    @classmethod
+    def from_dict(cls, d):
+        return Contract(Provider.from_dict(d.get('provider')), Consumer.from_dict(d.get('consumer')), 
+                Interaction.from_list(d.get('interactions')))
+
+class Provider:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def from_dict(cls, d):
+        return Provider(d.get('name'))
+
+class Consumer:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def from_dict(cls, d):
+        return Consumer(d.get('name'))
+
+class Interaction:
+    def __init__(self, provider_state, description, service, request=None, response=None):
+        self.provider_state = provider_state
+        self.description = description
+        self.service = service
+        self.request = request
+        self.response = response
+
+    @classmethod
+    def from_list(cls, a):
+        return list(map(lambda d: cls.from_dict(d), a))
+
+    @classmethod
+    def from_dict(cls, d):
+        return Interaction(d.get('providerState'), d.get('description'), d.get('service'), 
+                Request.from_dict(d.get('request')), Response.from_dict(d.get('response')))
+
+class Request:
+    def __init__(self, method, arguments=[]):
+        self.method = method
+        self.arguments = arguments
+
+    @classmethod
+    def from_dict(cls, d):
+        return Request(d.get('method'), d.get('arguments'))
+
+class Response:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_dict(cls, d):
+        return Response(d.get('value'))
+
 class ServiceContract:
     """
     function_contracts is a map from function name (Thrift doesn't allow overloading) to a
--- tests/contracts_test.py
+++ tests/contracts_test.py
@@ -1,5 +1,6 @@
 from ptcdt.contracts import *
 from ptcdt.matchers import *
+import utils
 
 def test_service_contract_simple_string():
     expected_params = ["first"]
@@ -17,3 +18,21 @@ def test_service_contract_simple_struct():
     service_contract = ServiceContract({"testFunction": function_contract})
 
     assert service_contract.match_contract("testFunction", expected_params) == "result"
+    
+def test_simple_contract_parsing():
+    filename = utils.test_resource_path("contracts/simple.tpact")
+    contract = parse_contract(filename)
+    assert contract.provider.name == "ThriftTest"
+    assert contract.consumer.name == "ThriftTestConsumer"
+    assert len(contract.interactions) == 1
+    interaction = contract.interactions[0]
+    assert interaction.provider_state == "Test struct constant returns"
+    assert interaction.description == "Description"
+    assert interaction.service == "ThriftTest"
+    assert interaction.request.method == "testStruct"
+    assert len(interaction.request.arguments) == 1
+    assert interaction.request.arguments[0] == {"string_thing": "string_thingy", 
+            "byte_thing": 0, "i16_thing": 16, "i32_thing": 32, "i64_thing": 64}
+    assert interaction.response.value == {"string_thing": "string_thingy2", 
+            "byte_thing": 1, "i16_thing": 17, "i32_thing": 33, "i64_thing": 65}
+
